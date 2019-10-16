f923126a7a5d0cfec08730d4ace523be6b5181a5
CorentinDieudonne <corentin.dieudonne@gmail.com>
2019-10-16 16:42:10 +0200

    Add the class for the test

---
+++ Unit Test/Controller/EngineTest.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EngineTest {
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void generateTicket() {
+    }
+
+    @Test
+    void callNextCustomer() {
+    }
+}
