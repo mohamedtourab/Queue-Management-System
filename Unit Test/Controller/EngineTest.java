134be9ccb5daf18bcfe14416187a5eb775ae4262
CorentinDieudonne <corentin.dieudonne@gmail.com>
2019-10-18 17:01:08 +0200

    adding the first test

--- Unit Test/Controller/EngineTest.java
+++ Unit Test/Controller/EngineTest.java
@@ -1,20 +1,57 @@
 package Controller;
 
+import Model.Counter;
+import Model.Service;
+import Model.Ticket;
+import Controller.Engine;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Queue;
+import java.util.LinkedList;
+
+import static Model.Service.TypeOfService.ACCOUNT;
+import static Model.Service.TypeOfService.PACKAGE;
 import static org.junit.jupiter.api.Assertions.*;
 
-class EngineTest {
+class EngineTest{
 
+    Engine engine = new Engine();
     @Test
-    void init() {
+    @DisplayName("Need")
+        //Verify that the ticket that has all its characteristics
+    Ticket generateTicket2(Service.TypeOfService accountOne) {
+
+        //SETUP
+
+        Service serviceAccount = new Service(ACCOUNT);
+        Service servicePackage = new Service (PACKAGE);
+
+        //ACTION
+        Ticket testOne=engine.generateTicket(serviceAccount.getServiceType());
+        engine.generateTicket(servicePackage.getServiceType());
+        //TEST
+        assertEquals(1,engine.getPackageQueue().size());
+        assertEquals(1,engine.getAccountQueue().size());
+        assertEquals(accountOne,testOne.getTypeOfService());
+        assertEquals(1,testOne.getTicketNumber());
+        return (testOne);
+
     }
 
     @Test
-    void generateTicket() {
-    }
-
-    @Test
+    @DisplayName("Need")
+        // Verify that the method call the next customer
     void callNextCustomer() {
+
+        //SETUP
+
+        //ACTION
+
+        //TEST
     }
 }
