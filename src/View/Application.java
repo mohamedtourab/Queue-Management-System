6738f52d97453a300a7824ba34d18ce14ddb62cf
Mohamed Mamdouh <33672420+MohamedMamdouh95@users.noreply.github.com>
2019-10-25 17:00:29 +0200

    adding the slides

--- src/View/Application.java
+++ src/View/Application.java
@@ -2,7 +2,6 @@ package View;
 
 public class Application {
     public static void main(String[] args) {
-
         OperatorUI operatorUI = new OperatorUI();
         CounterUI counterUI = new CounterUI();
         CallScreenGUI callScreenGUI = new CallScreenGUI();
